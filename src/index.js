52bb31421c058d7cffc8902266f4d7afc43f2f77
Tainá S. Lima <140896366+taina-s-lima@users.noreply.github.com>
2024-10-30 20:34:47 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('Hello Woooooooooooooorld');
+})
+
+
+
+
+app.listen(5000, () => {
+    console.log('Servidor rodando em http://localhost:5000');
+})
