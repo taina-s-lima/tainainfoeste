18eebf67220208ea794a8957897481261b580f2a
limaStaina <taina.souza@futcorp.com.br>
2024-10-31 15:00:17 -0300

    att

---
+++ src/db/database.js
@@ -0,0 +1,58 @@
+import mysql from 'mysql2';
+
+export default class Database {
+
+    #conexao;
+
+    get conexao() { return this.#conexao; } set conexao(conexao) { this.#conexao = conexao; }
+
+    constructor() {
+
+        this.#conexao = mysql.createPool({
+            host: 'localhost', //endereço do nosso banco de dados na nuvem
+            database: 'infoeste', //a database de cada um de vocês possui a nomenclatura DB_(RA)
+            user: 'root', // usuario e senha de cada um de vocês é o RA
+            password: '',
+        });
+    }
+
+    ExecutaComando(sql, valores) {
+        var cnn = this.#conexao;
+        return new Promise(function (res, rej) {
+            cnn.query(sql, valores, function (error, results, fields) {
+                if (error)
+                    rej(error);
+                else
+                    res(results);
+            });
+        })
+    }
+
+    ExecutaComandoNonQuery(sql, valores) {
+        var cnn = this.#conexao;
+        return new Promise(function (res, rej) {
+            cnn.query(sql, valores, function (error, results, fields) {
+                if (error)
+                    rej(error);
+                else
+                    res(results.affectedRows > 0);
+            });
+        })
+    }
+
+    ExecutaComandoLastInserted(sql, valores) {
+        var cnn = this.#conexao;
+        return new Promise(function (res, rej) {
+            cnn.query(sql, valores, function (error, results, fields) {
+                if (error)
+                    rej(error);
+                else
+                    res(results.insertId);
+            });
+        })
+    }
+
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
+let usuarios = [
+  { id: 1, nome: "João", email: "joao@email.com", idade: 25 },
+  { id: 2, nome: "João Pedro", email: "joao2@email.com", idade: 20 },
+  { id: 3, nome: "Paulo", email: "paulo@email.com", idade: 37 },
+  { id: 4, nome: "Marcão", email: "marcos@email.com", idade: 29 },
+];
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send('Hello Woooooooooooooorld');
-})
+  res.send("Hello FIPP 2");
+});
 
+app.get("/usuarios", (req, res) => {
+  res.status(200).json({ succes: true, data: usuarios });
+});
 
-APP
+app.get("/usuarios/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(400).json({ succes: false, message: "Manda o ID" });
+  } else {
+    const usuarioEncontrado = usuarios.find(
+      (usuario) => usuario.id === Number(id)
+    );
+
+    if (usuarioEncontrado !== undefined) {
+      res.status(200).json({
+        succes: true,
+        data: usuarioEncontrado,
+      });
+    } else {
+      res.status(404).json({
+        succes: false,
+        message: "Usuário não encontrado",
+      });
+    }
+  }
+});
+
+app.post("/usuarios", (req, res) => {
+  const { nome, email, idade } = req.body;
+
+  if (!nome || !email || !idade) {
+    res.status(400).json({
+      success: false,
+      message: "Informações inválidas",
+    });
+  } else {
+    const novoUsuario = {
+      id: usuarios[usuarios.length - 1].id + 1,
+      nome,
+      email,
+      idade,
+    };
+
+    usuarios.push(novoUsuario);
+
+    res
+      .status(200)
+      .json({ sucess: true, message: "Usuario criado com sucesso!" });
+  }
+});
+
 app.listen(5000, () => {
-    console.log('Servidor rodando em http://localhost:5000');
-})
+  console.log("Servidor rodando em http://localhost:5000");
+});
